e928847547c5da097b282ec52bcb2d4ff6ec14e4
KausTarafdar <kaustarafdar@gmail.com>
2024-11-12 14:05:48 +0530

    Feature:Add Login Route

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,22 +1,43 @@
 from fastapi import FastAPI, APIRouter
-from db.models import UserInfo
+from fastapi.security import OAuth2PasswordBearer
 
-app = FastAPI()
-router = APIRouter()
+from ..db import models
+from ..core.authenticate_user import authenticate_user
+from ..core.token import generate_jwt_token
+from ..utils.response_helpers import success_response, error_response
+
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth']
+)
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 '''Login to generate JWT for user session'''
 @router.post("/login/")
-async def login_user(user: UserInfo):
-    # check for jwt not existing
-    # validate password
-    # set jwt
-    # return logged in status
-    pass
+async def login_user(user: models.UserInfo):
+    auth_user = authenticate_user(user)
+    if not auth_user:
+        return error_response(
+            error_type="Unauthorized",
+            details="Authentication Error",
+            message="Email or Password is wrong",
+            code=401
+        )
+
+    access_token = generate_jwt_token(user=auth_user)
+    print(access_token)
+    return success_response(
+        data={
+            'access_token': access_token,
+            'token_type': 'bearer'
+        },
+        message="User Authenticated",
+        code=202
+    )
+
 
 '''Logout route to expire user session'''
 @router.get("/logout/")
 async def logout_user():
-    # validate jwt
-    # expire jwt
-    # return logged out status
     pass
